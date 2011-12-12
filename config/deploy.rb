115dc50e01017e1502638e4afdb11f7ffdc2c067
Etienne Garnier <garnier.etienne@gmail.com>
2011-12-12 03:16:09 +0100

    update databases informations

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,25 @@
+require "bundler/capistrano"
+set :bundle_without,  [:development, :test]
+
+# rbenv
+set :default_environment, {
+  'PATH' => "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH"
+}
+
 set :application, "rails"
-set :repository,  ""
+set :repository,  "git@github.com:garnieretienne/rails-testing.git"
+set :branch, "master"
 
 set :scm, :git
+set :deploy_via, :remote_cache
+ssh_options[:forward_agent] = true
+set :deploy_to, "/var/app/#{application}"
+set :use_sudo, false
+set :user, "kurt"
 
 role :web, "testing.yuweb.com"
 role :app, "testing.yuweb.com"
 role :db,  "testing.yuweb.com", :primary => true
-role :db,  "your slave db-server here"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
